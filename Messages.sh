d0f9c11ea25b92c8f1a68c195718a9490ac9339f
Pino Toscano <pino@kde.org>
2009-03-23 16:19:02 +0000

    make sure to extract messages from the subdirectories too
    svn path=/trunk/playground/base/plasma/applets/lionmail/; revision=943298

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT *.cpp -o $podir/plasma_applet_lionmail.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/plasma_applet_lionmail.pot
 rm -f rc.cpp
